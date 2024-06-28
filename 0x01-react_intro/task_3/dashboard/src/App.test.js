eff19a626d5bf154d2e9c335ec33f1c8dbcfea98
youssefberrk <yberrakouan.lol@gmail.com>
2024-06-28 14:36:26 +0100

    Doneit

--- 0x01-react_intro/task_3/dashboard/src/App.test.js
+++ 0x01-react_intro/task_3/dashboard/src/App.test.js
@@ -6,4 +6,24 @@ import App from './App';
 it('renders without crashing', () => {
   shallow(<App />);
 });
+describe("App Componeent Tests", () => {
+  it("Renders without crashing", () => {
+    const app = shallow(<App />);
+    expect(app).toBeDefined();
+  });
+  it("renders a div with the class App-header", () => {
+    const app = shallow(<App />);
 
+    expect(app.find(".App-header")).toBeDefined();
+  });
+  it("renders a div with the class App-body", () => {
+    const app = shallow(<App />);
+
+    expect(app.find(".App-body")).toBeDefined();
+  });
+  it("renders a div with the class App-footer", () => {
+    const app = shallow(<App />);
+
+    expect(app.find(".App-footer")).toBeDefined();
+  });
+});
